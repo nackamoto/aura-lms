a58805bb2678f80658f4609f4ee74826682d5b1d
ofoetey <denfu.oscar.dev@gmail.com>
2024-08-15 13:44:18 +0000

    Update logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const Logo = () => {
-  return <Image src="/logo.svg" alt="Logo" width={130} height={130} />;
+  return <Image src="/logo.svg" alt="Logo" width={50} height={50} />;
 };
 
 export default Logo;
